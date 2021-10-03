34657cdf9db1e76ccd100485a488f74edcf384fc
chaenabi <find1086@gmail.com>
2021-10-04 00:47:28 +0900

    Initial commit

---
+++ src/main/java/io/security/basicsecurity/BasicSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package io.security.basicsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicSpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicSpringSecurityApplication.class, args);
+    }
+
+}
