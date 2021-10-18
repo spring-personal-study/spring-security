2d08bd75b5ecdfa9bb11fadbd516dc0291b7c39f
chaenabi <find1086@gmail.com>
2021-10-18 14:21:02 +0900

    실전 프로젝트 생성. (이 커밋을 받아서 연습에 사용하면 됨)

--- src/main/java/io/security/basicsecurity/security/configs/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/security/configs/SecurityConfig.java
@@ -1,34 +1,63 @@
 package io.security.basicsecurity.security.configs;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.FilterInvocation;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+
+    /**
+     * WebIgnore
+     * js / css / image 파일 등 보안 필터를 적용할 필요가 없는 리소스를 설정.
+     *
+     * web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+     * static 파일들에 대한 경로에 대해서는 보안 필터를 거치지 않음
+     *
+     * .antMatchers("/static/").permitAll() 과의 차이점은, antMatchers().permitAll()은 일단 보안필터를 한번 거친다는 것.
+     * web.ignoring()은 보안필터 자체를 거치지 않으므로 비용적인 면에서 낫다.
+     *
+     * @see org.springframework.security.web.access.intercept.FilterSecurityInterceptor#invoke(FilterInvocation)
+     */
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
+    /**
+     * 비밀번호 암호화 설정
+     */
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
+    /**
+     * 인메모리 유저 생성
+     */
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
         String password = passwordEncoder().encode("1111");
 
         auth.inMemoryAuthentication().withUser("user").password(password).roles("USER");
-        auth.inMemoryAuthentication().withUser("manager").password(password).roles("MANAGER");
-        auth.inMemoryAuthentication().withUser("admin").password(password).roles("ADMIN");
-
+        auth.inMemoryAuthentication().withUser("manager").password(password).roles("MANAGER", "USER");
+        auth.inMemoryAuthentication().withUser("admin").password(password).roles("ADMIN", "USER", "MANAGER");
     }
 
+    /**
+     * security 설정
+     */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
