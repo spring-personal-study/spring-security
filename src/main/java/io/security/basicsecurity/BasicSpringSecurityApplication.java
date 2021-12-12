ff8f35f0906045fe4763c47ca95ec1b707ad1bf3
chaenabi <find1086@gmail.com>
2021-12-12 10:26:35 +0900

    move this repository to organization

--- src/main/java/io/security/basicsecurity/BasicSpringSecurityApplication.java
+++ src/main/java/io/security/basicsecurity/BasicSpringSecurityApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BasicSpringSecurityApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BasicSpringSecurityApplication.class, args);
     }
-
 }
